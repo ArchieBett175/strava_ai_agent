1efb412dbe46339d6d80bcf0a984116c9f2131da
Archiebett <archiebett@hotmail.co.uk>
2025-08-11 23:04:09 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ from GatherData import main as stravaApi_main
 from GeminiRunningDataAnalyzer import main as dataAnalyzer_main
 from assignCalendarEvent import main as assignCalEvent_main
 
+
 def main():
-    
+    print("main")
